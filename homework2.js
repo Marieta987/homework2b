e2bf563dae8c4205a1cd6d4d63fd97dc8fb63c3e
Marieta <marietabaghdasaryan4@gmail.com>
2018-09-13 16:20:22 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,30 @@
+
+const pizza = 50;
+const cola = 15;
+const frees = 20;
+const customerHas = 35;
+const bill = 2;
+alert ("Welcome to my pizzeria");
+
+
+if (customerHas >= 50) {
+    console.log("heey you can have pizza " );   }
+
+else if (20<customerHas<50){
+    console.log("frees or 2 colas?");
+    console.log((frees + bill)+ "or" +(2 * cola + bill)) ;
+
+}
+
+else if(customerHas = 17){console.log("colaa");
+}
+
+else{console.log("krkin pordzir!");}
+
+
+
+
+
+
+
+
